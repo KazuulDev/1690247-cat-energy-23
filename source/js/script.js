34667e88288bc0a9bc893a95a26a6a6df318bbc2
firefoxic <firefoxic.dev@gmail.com>
2021-09-14 23:04:39 +0300

    nav

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,15 @@
-let navigation = document.querySelector('.site-navigation');
-let navigationButton = document.querySelector('.site-navigation__toggle');
+let navigation = document.querySelector(".site-navigation");
+let navigationButton = document.querySelector(".site-navigation__toggle");
 
-navigationButton.addEventListener('click', function() {
-  if (navigation.classList.contains('site-navigation--closed')) {
-    navigation.classList.remove('site-navigationu--closed');
-    navigationButton.classList.add('site-navigation--opened');
+navigation.classList.remove("site-navigation--nojs");
+navigation.classList.add("site-navigation--closed");
+
+navigationButton.addEventListener("click", function () {
+  if (navigation.classList.contains("site-navigation--closed")) {
+    navigation.classList.remove("site-navigation--closed");
+    navigation.classList.add("site-navigation--opened");
   } else {
-    navigation.classList.add('site-navigation--closed');
-    navigation.classList.remove('site-navigation--opened');
+    navigation.classList.add("site-navigation--closed");
+    navigation.classList.remove("site-navigation--opened");
   }
 });
